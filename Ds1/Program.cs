8be0877c53ad59d9a4e2414822951104e7832bd9
Nikita <Nikitasmirnov2000@yandex.ru>
2022-08-27 12:23:01 +0300

    Ds1

---
+++ Ds1/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Создан массив из 5 чисел");
+int size = 5;
+int[] numbers = new int[size];
+for(int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(1,200);
+}
+
+void WriteArray(int[] numbers)
+{
+    Console.Write("[");
+    int i = 0;
+    int count = numbers.Length;
+
+    while (i < count)
+    {
+        Console.Write(numbers[i]); i++;
+        if (i < count)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+Console.Write("Результат формирования массива: ");
+WriteArray(numbers);
+
+int count = 0;
+for(int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i] > 10 && numbers[i] < 99) count++;
+}
+
+Console.Write($"Cумма элементов с позициями от 10 до 99: {count}");
